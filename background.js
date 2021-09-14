edf9d0f85b7548f7ffda028b7737477e6b03ce1f
Jovan Turanjanin <jovan@turanjanin.net>
2021-09-14 15:34:49 +0200

    Add magazin.novosti.rs, video.novosti.rs and www.euronews.rs to transliteration list

--- background.js
+++ background.js
@@ -31,6 +31,8 @@ var redirects = [
                                                                          , { match: '^(https?)://www.kragujevac.rs/((?!.*\?script=(?:cir|lat)).*)',   redirect: '$1://www.kragujevac.rs/$2?script=cir'        } ] }
     , { enabled: false, filter: '*://www.mod.gov.rs/*',           rules: [ { match: '^(https?)://www.mod.gov.rs/lat(.*)$',                            redirect: '$1://www.mod.gov.rs/cir$2'                   } ] }
     , { enabled: false, filter: '*://www.novosti.rs/*',           rules: [ { match: '^(https?)://www.novosti.rs/(?!c/)(.*?)$',                        redirect: '$1://www.novosti.rs/c/$2'                    } ] }
+    , { enabled: false, filter: '*://www.magazin.novosti.rs/*',   rules: [ { match: '^(https?)://www.magazin.novosti.rs/$',                           redirect: '$1://www.magazin.novosti.rs/scc/'            }
+                                                                         , { match: '^(https?)://www.magazin.novosti.rs/sr/(.*?)$',                   redirect: '$1://www.magazin.novosti.rs/scc/$2'          } ] }
     , { enabled: false, filter: '*://www.nspm.rs/*',              rules: [ { match: '^(https?)://www.nspm.rs/(.*)?alphabet=l$',                       redirect: '$1://www.nspm.rs/$2?alphabet=c'              } ] }
     , { enabled: false, filter: '*://www.politika.rs/*',          rules: [ { match: '^(https?)://www.politika.rs/sr(/?)(.*)$',                        redirect: '$1://www.politika.rs/scc$2$3'                } ] }
     , { enabled: false, filter: '*://posta.rs/*',                 rules: [ { match: '^(https?)://(?:.*\.)?posta.rs/lat/(.*)$',                        redirect: '$1://posta.rs/cir/$2'                        }
@@ -187,6 +189,7 @@ chrome.runtime.onInstalled.addListener(function () {
             'toppress.rs',
             'topzdravlje.rs',
             'ucentar.rs',
+            'video.novosti.rs',
             'vijestisrpske.com',
             'vojvodinainfo.rs',
             'vrbasmedia.com',
@@ -214,6 +217,7 @@ chrome.runtime.onInstalled.addListener(function () {
             'www.economy.rs',
             'www.ekapija.com',
             'www.espreso.rs',
+            'www.euronews.rs',
             'www.glas-javnosti.rs',
             'www.glas-zajecara.com',
             'www.glasamerike.net',
